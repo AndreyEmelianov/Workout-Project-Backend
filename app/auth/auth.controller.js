9da22b7c82600d7829a043d411f2fec67f361f3c
Andrey Emelianov <621077@mail.ru>
2023-08-18 19:01:45 +0300

    подключил призму и законнектился с бд postgresql

--- app/auth/auth.controller.js
+++ app/auth/auth.controller.js
@@ -1,7 +1,9 @@
+import { Prisma } from '../prisma.js';
 //@desc Auth user
 //@route POST /api/auth/login
 //@access Public
 
 export const authUser = async (req, res) => {
-	res.json({ message: 'You are authenticated' });
+	const user = await Prisma.user.findMany();
+	res.json(user);
 };
