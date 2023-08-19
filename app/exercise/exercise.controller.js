24b70a44963ef8c82b5e77b477f2dee815229aad
Andrey Emelianov <621077@mail.ru>
2023-08-19 19:20:39 +0300

    реализовал в контроллере и путях упражнений обновленние данных по упражнениям и удаление

--- app/exercise/exercise.controller.js
+++ app/exercise/exercise.controller.js
@@ -29,3 +29,48 @@ export const getExercises = asyncHandler(async (req, res) => {
 
 	res.json(exercises);
 });
+
+//@desc  Update exercise
+//@route  PUT api/exercise/:id
+//@access Private
+
+export const updateExercise = asyncHandler(async (req, res) => {
+	const { name, times, iconPath } = req.body;
+
+	try {
+		const exercise = await Prisma.exercise.update({
+			where: {
+				id: Number(req.params.id)
+			},
+			data: {
+				name,
+				times,
+				iconPath
+			}
+		});
+
+		res.json(exercise);
+	} catch (error) {
+		res.status(404);
+		throw new Error('Exercise not found!');
+	}
+});
+
+//@desc  Delete exercise
+//@route  DELETE api/exercise/:id
+//@access Private
+
+export const deleteExercise = asyncHandler(async (req, res) => {
+	try {
+		const exercise = await Prisma.exercise.delete({
+			where: {
+				id: Number(req.params.id)
+			}
+		});
+
+		res.json({ message: `Exercise deleted!` });
+	} catch (error) {
+		res.status(404);
+		throw new Error('Exercise not found!');
+	}
+});
